b7cbd97f2958fa01617baae00c66fe962512007a
yezune choi <yezune@gmail.com>
2012-04-16 22:33:01 +0900

    refine agent

--- agent/fu.node.js
+++ agent/fu.node.js
@@ -29,6 +29,8 @@ var fs 				= require("fs");
 var sys 			= require("sys");
 var url 			= require("url");
 var path 			= require('path');
+var	spawn 			= require('child_process').spawn,
+
 
 DEBUG 				= false;
 
@@ -143,40 +145,6 @@ fu.staticHandler = function (filename) {
     });
   }
 };
-
-//https://gist.github.com/742162
-fu.mkdir_p =  function (path, mode, callback, position) {
-    mode = mode || 0777;
-    position = position || 0;
-    parts = require('path').normalize(path).split('/');
-
-    if (position >= parts.length) {
-        if (callback) {
-            return callback();
-        } else {
-            return true;
-        }
-    }
-
-    var directory = parts.slice(0, position + 1).join('/');
-    fs.stat(directory, function(err) {
-        if (err === null) {
-            mkdir_p(path, mode, callback, position + 1);
-        } else {
-            fs.mkdir(directory, mode, function (err) {
-                if (err) {
-                    if (callback) {
-                        return callback(err);
-                    } else {
-                        throw err;
-                    }
-                } else {
-                    mkdir_p(path, mode, callback, position + 1);
-                }
-            })
-        }
-    })
-}
 
 // stolen from jack- thanks
 fu.mime = {
